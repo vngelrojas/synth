0da6fc9564317639e6848f7beb0dd01d7d5c39f5
Angel Rojas <aarojas@usc.edu>
2023-07-03 15:15:59 -0700

    mongodb setup

--- server/server.js
+++ server/server.js
@@ -7,3 +7,12 @@ dotenv.config();
 
 
 // Mongoose setup
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL,
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>
+{
+    app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+}).catch((error) => console.log(`${error}`));
